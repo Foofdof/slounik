c5a3879d92ee7fab269ca264137645ca6064bca3
Foofdof <foofdof@gmail.com>
2020-11-07 19:11:09 +0300

    End

--- TransSite/views.py
+++ TransSite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Word
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+def post_list(request):
+    posts = Word.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'TransSite/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Word, pk=pk)
+    return render(request, 'TransSite/post_detail.html', {'post': post})
